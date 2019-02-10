2fdd5b8ee6448a37e3b2202231106ab3e6257baf
IvanBisagne123 <47418161+IvanBisagne123@users.noreply.github.com>
2019-02-10 15:44:52 -0300

    tarea 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+    var precio, descuento, porsentaje_descuento, precio_final,
+    precio= prompt ("Ingrese el precio");
+    precio= parseInt (precio);
+    
+    porcentaje_descuento= prompt ("Ingrese el porsentaje de descuento");
+    porsentaje_descuento= parseInt (porsentaje_descuento);
 
-}
+    descuento= porsentaje_descuento *precio /100;
+    alert("el porsentaje del descuento es " + descuento);
+
+    precio_final= precio - descuento;
+    alert("el precio con el descuento  es " +precio_final);
+}    
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,7 @@
 function mostrar()
 {
-  
+    var nombre, localidad;
+    nombre= document.getElementById ("elNombre").value;
+    localidad= document.getElementById ("laLocalidad").value;
+    alert("su nombre es  " +nombre+ " y su localidad es " +localidad);
 }
